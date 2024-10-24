e42c48dd7331f4e6db1207a99069f9ebe305c9c4
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2024-10-24 22:18:03 +0200

    Script to render and remove unnecessary bits

---
+++ render_and_clean_up_html.R
@@ -0,0 +1,20 @@
+library(fs)
+library(purrr)
+library(rvest)
+library(xml2)
+
+
+remove_ahnang <- function(filename) {
+  html_file <- read_html(filename)
+
+  extra_nodes <-
+    html_file |>
+    html_nodes(xpath = "//li[@class='sidebar-item sidebar-item-section' and contains(., 'Anhang')]")
+
+  xml_remove(extra_nodes)
+
+  write_html(html_file, filename)
+}
+
+babelquarto::render_book()
+purrr::walk(fs::dir_ls("_book/de", glob = "*de.html"), ~remove_ahnang(.))
