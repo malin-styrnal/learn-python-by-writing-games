5bc1b17c1a2ff4848e2659346816f50d1e270b7d
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2024-10-26 11:04:17 +0200

    Update English + German translation

--- render_and_clean_up_html.R
+++ render_and_clean_up_html.R
@@ -7,14 +7,23 @@ library(xml2)
 remove_ahnang <- function(filename) {
   html_file <- read_html(filename)
 
+  # remove Anhang (appears in the German version as an extra text in the Content)
   extra_nodes <-
     html_file |>
     html_nodes(xpath = "//li[@class='sidebar-item sidebar-item-section' and contains(., 'Anhang')]")
-
   xml_remove(extra_nodes)
+
+
+  # add language-selector class to the drop down
+  language_btn <-
+    html_file |>
+    html_nodes(xpath = '//*[@id="languages-button"]')
+  xml_attr(language_btn, "class") <- paste(c(xml_attr(language_btn, "class"), "language-selector"), collapse = " ")
 
   write_html(html_file, filename)
 }
 
 babelquarto::render_book()
 purrr::walk(fs::dir_ls("_book/de", glob = "*de.html"), ~remove_ahnang(.))
+purrr::walk(fs::dir_ls("_book/", glob = "*.html"), ~remove_ahnang(.))
+
